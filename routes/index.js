08da45a3c740db96484e348e3aa8f4cde429c518
Yiming <yanyiming@dhcp-gs-10592.eduroam.cornell.edu>
2018-10-12 12:43:53 -0400

    Update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,6 @@ var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
-//__________________________________________________________
-var atHome=new Boolean(true);
-//__________________________________________________________
-
 //root route
 router.get("/", function(req, res){
     res.render("landing");
